68c7ea80b9a3758c62c0bcb7e4ef45e2336ba6b2
RevanthGovindan <revanthji14@gmail.com>
2019-09-30 11:58:29 +0530

    log path change

--- helpers/log4j.js
+++ helpers/log4j.js
@@ -5,7 +5,7 @@ var name = date.getDate()+"-"+date.getMonth()+"-"+date.getFullYear();
 log4js.configure({
     appenders: { data: { 
         type: 'file', 
-        filename: `${name}.log`,
+        filename: `logs/${name}.log`,
         maxLogSize : 1073741824
     } },
     categories: { default: { appenders: ['data'], level: 'debug' } }
